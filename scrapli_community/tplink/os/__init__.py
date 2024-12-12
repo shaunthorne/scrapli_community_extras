343bf12125b2e885ee7d343d6b54ce9036fb046d
shaunthorne <a.mu@ro.ru>
2024-12-13 01:55:54 +0300

    added tplink_os platform

---
+++ scrapli_community/tplink/os/__init__.py
@@ -0,0 +1,4 @@
+"""scrapli_community.tplink.os"""
+from scrapli_community.tplink.os.tplink_os import SCRAPLI_PLATFORM
+
+__all__ = ("SCRAPLI_PLATFORM",)
--- scrapli_community/tplink/os/sync_driver.py
+++ scrapli_community/tplink/os/sync_driver.py
@@ -1,10 +1,10 @@
-"""scrapli_community.dlink.os.sync_driver"""
+"""scrapli_community.tplink.os.sync_driver"""
 from scrapli.driver import NetworkDriver
 
 
 def default_sync_on_open(conn: NetworkDriver) -> None:
     """
-    Default dlink_os on_open callable
+    snr_esr on_open callable
 
     Args:
         conn: NetworkDriver object
@@ -16,14 +16,14 @@ def default_sync_on_open(conn: NetworkDriver) -> None:
         N/A
 
     """
+    print('123123123')
     conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
-    conn.send_command(command="disable clipaging")
-    print('test')
+    conn.send_command(command="terminal length 0")
 
 
 def default_sync_on_close(conn: NetworkDriver) -> None:
     """
-    Default dlink_os on_close callable
+    snr_esr default on_close callable
 
     Args:
         conn: NetworkDriver object
@@ -38,6 +38,5 @@ def default_sync_on_close(conn: NetworkDriver) -> None:
     # write exit directly to the transport as channel would fail to find the prompt after sending
     # the exit command!
     conn.acquire_priv(desired_priv=conn.default_desired_privilege_level)
-    conn.send_command(command="enable clipaging")
-    conn.channel.write(channel_input="logout")
+    conn.channel.write(channel_input="exit")
     conn.channel.send_return()
